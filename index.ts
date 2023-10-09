4e37b864492c57d657bacdd1f76dc785d75f1c78
Impulse <iimpulseehd@gmail.com>
2023-10-09 20:08:50 +0200

    Adding the project CLI

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
+import { createNodeFromGenesis } from "./src/createNodeFromGenesis";
 import { projectVersion, projectName } from "./config";
 import figlet from "figlet";
 
 (() => {
-    console.clear();
     const args: string[] = process.argv.slice(2, process.argv.length);
     const ipv4Regex: RegExp = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
 
     if (args.length === 1 && args[0] === "createNodeFromGenesis") {
-        console.log(args);
+        createNodeFromGenesis();
     } else if (args.length === 1 && args[0] === "createNode") {
         console.log(args);
     } else if (args.length === 2 && args[0] === "joinNode" && args[1] && ipv4Regex.test(args[1])) {
